ffd4de3aa298286c389fa520828800db7a0631cc
rpawlik <ramsey.pawlik@rackspace.com>
2012-10-18 18:37:04 -0500

    added chef file backup and cleaned thing up a bit

--- blackhole-bu.sh
+++ blackhole-bu.sh
@@ -20,11 +20,12 @@ backupdir="/backups/"
 # 1 day = 1440
 # 1 week = 10080
 # 30 days = 43200
-mins="30"
+mins="300"
 
 ################################################################
 
 
+#backup chef server VM
 if [ ! -d $backupdir ]
 then
   echo "$backupdir does not exist, creating."
@@ -40,7 +41,7 @@ then
   then
     while ps auwx | grep $chefvm | grep -v grep | awk {'print $21'} | grep $chefvm >/dev/null
     do
-      echo "Shutting down $chefvm VM"
+      echo "Shutting down $chefvm VM."
       ssh rack@$chefip 'sudo shutdown -h now'
       sleep 10
     done
@@ -48,9 +49,9 @@ then
     then
       echo "Copying Chef VM and compressing the image. This may take some time."
       cat $vmdiskloc | gzip > $backupdir$chefvm-backup.qcow2.gz
-      echo "Starting $chefvm"
+      echo "Starting $chefvm VM."
       virsh start $chefvm >/dev/null
-      echo -e "$chefvm backup complete! Find it here: \n$backupdir$chefvm-backup.qcow2.gz"
+      echo -e "$chefvm VM backup complete! Find it here: $backupdir$chefvm-backup.qcow2.gz"
     else
       echo "$vmdiskloc does not exist!"
       exit 1
@@ -59,7 +60,34 @@ then
     echo "Chef server VM not running, doing nothing."
   fi
 else
-   echo "$chefvm backup newer than $mins minutes, skipping."
+   echo "$chefvm VM backup newer than $mins minutes, skipping."
+fi
+
+#get chef config files
+
+while ! ssh -q rack@$chefip 'hostname' >/dev/null
+do
+  echo "Waiting for Chef VM to start..."
+  sleep 10
+done
+
+if ssh rack@$chefip "find /home/rack/ -name  chef-backup-* -mmin $mins" >/dev/null
+then
+  echo "Shutting down chef-server and couchdb."
+  ssh rack@$chefip 'sudo service chef-server stop; sudo service couchdb stop; sudo service chef-expander stop' >/dev/null 
+  echo "Removing old Chef backup."
+  ssh rack@$chefip "rm -rf /home/rack/chef-backup-*"
+  rm -rf $backupdir"chef-backup-*"
+  echo "Creating new Chef backup. This may take some time."
+  ssh rack@$chefip 'sudo tar czPf chef-backup-`date +%Y-%m-%d-%s`.tar.gz /etc/couchdb /var/lib/chef /var/lib/couchdb /var/cache/chef /var/log/chef /var/log/couchdb /etc/chef'
+  echo "Copying Chef backup to $backupdir"
+  scp -q rack@$chefip:/home/rack/chef-backup-* $backupdir
+  echo "Starting chef-server and couchdb."
+  ssh rack@$chefip 'sudo service chef-server start; sudo service couchdb start; sudo service chef-expander start' >/dev/null
+  echo "Chef file and couchdb backup complete. Find it here: `ls $backupdir'chef-backup-'*`"
+else
+  echo "Chef file backup newer than $mins minutes, skipping."
 fi
 
 
+ 
